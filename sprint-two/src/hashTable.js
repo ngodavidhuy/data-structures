1f78b2c8088cd59e02fb7a96c38b5591e3aab172
ngodavidhuy <ngodavidhuy@gmail.com>
2018-11-10 11:01:50 -0800

    Implements hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,40 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!Array.isArray(this._storage.get(index))) {
+    this._storage.set(index, [[k,v]]);
+  } else {
+    let bucket = this._storage.get(index);
+    for (let tuple of bucket) {
+      if (tuple[0] === k) {
+        tuple[1] = v; 
+      } else {
+        bucket.push([k, v]);
+      }
+    }
+    this._storage.set(index, bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  for (let tuple of bucket) {
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  for (let i = 0; i < bucket.length; i++) {
+    if(bucket[i][0] === k) {
+      bucket.splice(i, i + 1);
+    }
+  }
+  this._storage.set(index, bucket);
 };
 
 
@@ -22,5 +48,27 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+// HashTable.prototype.insert => O(n);
+// HashTable.prototype.retrieve => O(n)
+// HashTable.prototype.remove => O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
