dd81d609b20c0c5110b64eda8ecd41238d2bd275
ngodavidhuy <ngodavidhuy@gmail.com>
2018-11-10 17:32:50 -0800

    Implements binarySearchTree.js.
    depthFirstLog function is in progress.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,75 @@
 var BinarySearchTree = function(value) {
+  let node = Object.create(binarySearchMethods);
+  node.value = value;
+  node.left = undefined;
+  node.right = undefined;
+  return node;
 };
+
+let binarySearchMethods = {
+  insert : function(value) {
+    let newNode = BinarySearchTree(value);
+    
+    let insertNode = function(node) {
+      if (node.value > value && !node.left) {
+        node.left = newNode;
+      } else if (node.value > value && node.left) {
+        insertNode(node.left);
+      } else if (node.value <= value && !node.right) {
+        node.right = newNode;
+      } else if (node.value <= value && node.right) {
+        insertNode(node.right);
+      }
+    }
+    
+    insertNode(this);
+  },
+
+  contains : function(target) {
+    let wasFound = false;
+ 
+    let traverse = function(node) {
+      if (node.value === target) {
+        console.log('target found');
+        wasFound = true;
+      } else if (node.value > target) {
+        if (node.left) {
+          traverse(node.left);
+        }
+      } else if (node.value <= target) {
+        console.log('target is greater than or equal to value of node')
+        if (node.right) {
+          traverse(node.right);
+        }
+      }
+    }
+    
+    traverse(this);
+    return wasFound;
+  },
+
+  depthFirstLog : function(cb) {
+
+    let diging = function(node) {
+      if (node.left) {
+        return cb(node.value);
+      }
+    }
+    
+    diging(this);
+    return diging(this);
+    
+  }
+}
+
+// binarySearchTree.insert(2);
+// binarySearchTree.insert(3);
+// binarySearchTree.insert(7);
+// expect(binarySearchTree.contains(7)).to.equal(true);
+
+
+
+
 
 
 /*
