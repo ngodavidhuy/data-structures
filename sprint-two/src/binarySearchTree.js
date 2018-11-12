325cede3c8bf4fe7dba6c1ae541f8674955d4cb1
David Ngo <ngo.david.huy@gmail.com>
2018-11-11 16:20:51 -0800

    Refactors binarySearchTree.js in pseudoclassical pattern

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,77 +1,63 @@
 var BinarySearchTree = function(value) {
-  let node = Object.create(binarySearchMethods);
-  node.value = value;
-  node.left = undefined;
-  node.right = undefined;
-  return node;
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
 };
 
-let binarySearchMethods = {
-  insert : function(value) {
-    let newNode = BinarySearchTree(value);
-    
-    let insertNode = function(node) {
-      if (node.value > value && !node.left) {
-        node.left = newNode;
-      } else if (node.value > value && node.left) {
-        insertNode(node.left);
-      } else if (node.value <= value && !node.right) {
-        node.right = newNode;
-      } else if (node.value <= value && node.right) {
-        insertNode(node.right);
-      }
+BinarySearchTree.prototype.insert = function(value) {
+  let newNode = new BinarySearchTree(value);
+
+  let insertNode = function(node) {
+    if (node.value > value && !node.left) {
+      node.left = newNode;
+    } else if (node.value > value && node.left) {
+      insertNode(node.left);
+    } else if (node.value <= value && !node.right) {
+      node.right = newNode;
+    } else if (node.value <= value && node.right) {
+      insertNode(node.right);
     }
-    
-    insertNode(this);
-  },
+  };
 
-  contains : function(target) {
-    let wasFound = false;
- 
-    let traverse = function(node) {
-      if (node.value === target) {
-        console.log('target found');
-        wasFound = true;
-      } else if (node.value > target) {
-        if (node.left) {
-          traverse(node.left);
-        }
-      } else if (node.value <= target) {
-        console.log('target is greater than or equal to value of node')
-        if (node.right) {
-          traverse(node.right);
-        }
-      }
+  insertNode(this);
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+  let traverse = function(node) {
+    if (node.value === target) {
+      return true;
+    } else if (node.value > target) {
+      return node.left ? traverse(node.left) : false;
+    } else if (node.value <= target) {
+      return node.right ? traverse(node.right) : false;
     }
-    
-    traverse(this);
-    return wasFound;
-  },
+  };
 
-  depthFirstLog : function(cb) {
+  return traverse(this);
+};
 
-    let diging = function(node) {
-      if (node.left) {
-        return cb(node.value);
-      }
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  let traverse = function (node) {
+    if (node.value) {
+      node.value = cb(node.value);
     }
-    
-    diging(this);
-    return diging(this);
-    
-  }
-}
+    if (node.left) {
+      traverse(node.left);
+    }
+    if (node.right) {
+      traverse(node.right);
+    }
+  };
 
-// binarySearchTree.insert(2);
-// binarySearchTree.insert(3);
-// binarySearchTree.insert(7);
-// expect(binarySearchTree.contains(7)).to.equal(true);
-
-
-
-
-
+  traverse(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ */
+
+ /*
+BinarySearchTree.prototype.insert ===> O(n)
+BinarySearchTree.prototype.contains ===> O(log n)
+BinarySearchTree.prototype.depthFirstLog ===> O(n)
  */
