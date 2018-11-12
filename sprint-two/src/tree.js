1bdcd110741bae32361257c849235468c2fa37ef
David Ngo <ngo.david.huy@gmail.com>
2018-11-11 16:24:18 -0800

    States time complexities of tree methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -33,4 +33,8 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ treeMethods.addChild ===> O(1)
+  treeMethods.contains ===> O(n)
+
  */
+
